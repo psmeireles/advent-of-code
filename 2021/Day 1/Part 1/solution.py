2f681196e47fb8ef9e9e4cde1b05af8accf69330
Pedro Sousa Meireles <psmeireles25@gmail.com>
2021-12-01 16:05:22 -0300

    2021 - Day 1 - Part 1

---
+++ 2021/Day 1/Part 1/solution.py
@@ -0,0 +1,11 @@
+file = open("input.txt", "r")
+
+input = []
+for line in file:
+    input.append(int(line))
+
+result = 0
+for i in range(1, len(input)):
+    if input[i] > input[i-1]:
+        result += 1
+print(result)
