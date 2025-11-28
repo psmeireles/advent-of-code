24059ee472c1d261d0b46d172634ab9b97af3e28
Pedro Sousa Meireles <psmeireles25@gmail.com>
2025-11-28 20:00:47 +0000

    Setup Day 1 projects

---
+++ 2025/Java/AdventOfCode/Day01/src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Main {
+    static void main() {
+        IO.println(String.format("Day 1!"));
+    }
+}
---
+++ 2025/Java/AdventOfCode/settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "AdventOfCode"
+include("Day01")
